fedb1da6abbc9d6f2f81d07bb467aaa8f58fd634
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-23 21:33:14 -0300

    :bug: fix: requirement 02 - creating user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
 const UserSchema = (sequelize, DataTypes) => {
   const UsersTable = sequelize.define('User', {
-    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
