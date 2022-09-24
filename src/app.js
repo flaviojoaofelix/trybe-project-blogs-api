73e54dfcd35cd64db0646c2b12166023531afc3a
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-23 21:29:29 -0300

    :sparkles: feat: requirement 03 - bind post /login - app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
-// ...
+// require('express-async-errors');
 
 const app = express();
-
 app.use(express.json());
 
-// ...
+const loginRoutes = require('./routes/login.router');
+const errorMiddleware = require('./middlewares/error.middleware');
+
+app.use('/login', loginRoutes);
+
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
