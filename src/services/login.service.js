003ea6e5cf23989124cb3e5a7df912df1a57634d
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-25 12:56:34 -0300

    :recycle: refactor: login service

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,20 +1,33 @@
 const { User } = require('../models');
+
 const { generateToken } = require('../utils/JWT');
 
-const login = async ({ email, password }) => {
-  const findUser = await User.findOne({
+const checkUserInfo = async ({ email, password }) => {
+  const checkUser = await User.findOne({
     attributes: ['id', 'displayName', 'email'],
     where: { email, password },
   });
 
-  if (!findUser) {
+  return checkUser;
+};
+
+const createToken = async ({ id, displayName, email }) => {
+  const token = generateToken({ id, displayName, email });
+
+  return { token };
+};
+
+const login = async ({ email, password }) => {
+  const checkUser = await checkUserInfo({ email, password });
+
+  if (!checkUser) {
     const error = { status: 400, message: 'Invalid fields' };
     throw error;
   }
 
-  const token = generateToken(findUser.dataValues);
+  const token = createToken(checkUser.dataValues);
 
-  return { token };
+  return token;
 };
 
 module.exports = {
